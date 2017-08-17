c33f1560254b0d04e69ec6c66fb730587b3b155b
EchoDemo <1761910310@qq.com>
2017-08-17 22:49:25 +0800

    Update A+B Problem.cpp

--- Naive/A+B Problem.cpp
+++ Naive/A+B Problem.cpp
@@ -12,3 +12,19 @@
 
 样例输出
 57
+
+c++语言：
+
+#include "stdafx.h"
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	int a, b;
+	scanf("%d%d", &a, &b);
+	printf("%d", a + b);
+	system("pause");
+	return 0;
+}
+
